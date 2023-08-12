3fa109066b81fa19fd5ce65cca6fe987a45231dd
Jan Kuźma <1jankuzma@gmail.com>
2023-08-12 23:26:26 +0200

    react tutorial

---
+++ learning-react-1/src/dev/previews.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import App from "../App.jsx";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/App">
+                <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/PaletteTree">
+                <PaletteTree/>
+            </ComponentPreview>
+            <ComponentPreview path="/ComponentPreviews">
+                <ComponentPreviews/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
